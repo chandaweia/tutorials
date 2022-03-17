32b0b231817a77c5d6965c2ddba6a2ee42b88dfb
Cuidi Wei <cwei@nimbnode28.nimbus.seas.gwu.edu>
2022-03-17 21:09:07 +0000

    send

--- exercises/basic2/send.py
+++ exercises/basic2/send.py
@@ -28,7 +28,8 @@ def main():
     iface = get_if()
 
     print("sending on interface %s to %s" % (iface, str(addr)))
-    pkt =  Ether(src=get_if_hwaddr(iface), dst='ff:ff:ff:ff:ff:ff')
+    #pkt =  Ether(src=get_if_hwaddr(iface), dst='ff:ff:ff:ff:ff:ff')  #68:05:ca:30:52:cc
+    pkt =  Ether(src=get_if_hwaddr(iface), dst='68:05:ca:30:52:cc')
     pkt = pkt /IP(dst=addr) / TCP(dport=1234, sport=random.randint(49152,65535)) / sys.argv[2]
     pkt.show2()
     sendp(pkt, iface=iface, verbose=False)
